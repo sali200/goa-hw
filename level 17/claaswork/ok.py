9b410eaa0fc9fe502977944f1187e8e3544bbfdf
sali200 <salomemoseshvili2014@gmail.com>
2024-08-09 17:51:59 +0400

    commit

---
+++ level 17/claaswork/ok.py
@@ -0,0 +1,1 @@
+#no classwork it was grop work
---
+++ level 17/homework/lol.py
@@ -0,0 +1,20 @@
+i=1
+for i in range(11):
+    print("goa is the best")
+
+
+
+A= 1
+while A < 11:
+  print(i)
+  A += 1
+
+
+  t= 1
+while t < 11:
+  print(i)
+  t += 1
+
+
+
+
